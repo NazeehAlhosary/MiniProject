fe733e69459831cb1d28c1ff94a68834b996bd6f
Mohammad Zreik <guszremo@student.gu.se>
2017-11-26 20:48:56 +0100

    Edit

--- src/librarysystem/LibrarySystem.java
+++ src/librarysystem/LibrarySystem.java
@@ -20,6 +20,6 @@ public class LibrarySystem {
      */
     public static void main(String[] args) {   
        //Tools.OpenForm(new FrmLogin());
-        Tools.OpenForm(new FrmLogin());
+        Tools.OpenForm(new Menu());
     }
 }
