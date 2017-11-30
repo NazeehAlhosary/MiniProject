90d54e0198cdea34efa83c11cd3d3cdf59fb1766
Mohammad Zreik <33487535+MohammadZreik@users.noreply.github.com>
2017-11-30 17:16:30 +0100

    Add files via upload

---
+++ src/Classes/Return.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import Connection.Connections;
+import Connection.Tools;
+
+/**
+ *
+ * @author User
+ */
+public class Return {
+    private int ISBN;
+     private String Status;
+      private int CardNumber;
+    
+     public void UpdateStatusAvailable(){
+    String update = "Update History set "
+            +"Status='Available',"
+            + " where ISBN=" +getISBN() +"AND CardNumber= "+getCardNumber() +";";
+     boolean check = Connections.RunNonQuery(update);
+     if (check){
+         Tools.MsgBoxInfo("Book "+getISBN()+" is now available", "Update Status");
+     }
+    }
+     public void UpdateStatusUnavailable(){
+    String update = "Update History set "
+            +"Status='Unavailable',"
+            + " where ISBN=" +getISBN() +"AND CardNumber= "+getCardNumber() +";";
+     boolean check = Connections.RunNonQuery(update);
+     if (check){
+         Tools.MsgBoxInfo("Book "+getISBN()+" is now unavailable", "Update Status");
+     }
+    }
+
+    
+    public int getISBN() {
+        return ISBN;
+    }
+
+    
+    public String getStatus() {
+        return Status;
+    }
+
+   
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+    public int getCardNumber() {
+        return CardNumber;
+    }
+    
+}
+     
+     
+
+
+
+
